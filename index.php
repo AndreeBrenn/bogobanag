a77d8cf0d5d2eb7465579baa922159ebc765311e
Rouel <50535192+astrolabio1417@users.noreply.github.com>
2020-02-19 20:02:29 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <title>Hello, world!</title>
+	<?php include 'header.php' ?>
+  </head>
+  <body>
+	<?php include 'navbar.php' ?>
+	<div class="container">
+
+		<h1>TITLE OF THE PAGE</h1>
+		Hello World
+
+
+		<?php readfile("text.txt"); ?>
+	</div>
+	<?php include 'footer.php' ?>
+  </body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,6 @@
+<nav class="navbar navbar-dark bg-dark">
+	<a class="navbar-brand" href="index.php">--HOMEPAGE--</a>
+	<div>
+		
+	</div>
+</nav>
