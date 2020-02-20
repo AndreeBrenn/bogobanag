78a342e330a9b450adbc9e7c80b8f0dc451b0416
Rouel <50535192+astrolabio1417@users.noreply.github.com>
2020-02-21 00:44:51 +0800

    add list

--- navbar.php
+++ navbar.php
@@ -1,6 +1,14 @@
-<nav class="navbar navbar-dark bg-dark">
-	<a class="navbar-brand" href="index.php">--HOMEPAGE--</a>
-	<div>
-		
+<nav class="navbar navbar-dark bg-dark navbar-expand-sm">
+	<a class="navbar-brand" href="index.php">Rouel Astrolabio</a>
+
+	<div class="collapse navbar-collapse" id="navbar-rouel">
+		<ul class="navbar-nav mr-auto">
+			<li class="nav-item">
+				<a class="nav-link" href="index.php">HomePage</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="index2.php">HomePage2</a>
+			</li>
+		</ul>
 	</div>
 </nav>
