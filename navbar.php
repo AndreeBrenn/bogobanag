44548c0123f77bb0cfb3b0a093e97262c2ac2c28
Rouel <50535192+astrolabio1417@users.noreply.github.com>
2020-02-23 14:04:02 +0800

    new image and menu

--- navbar.php
+++ navbar.php
@@ -1,6 +1,18 @@
-<nav class="navbar navbar-dark bg-dark">
-	<a class="navbar-brand" href="index.php">--HOMEPAGE--</a>
-	<div>
-		
-	</div>
+<nav class="navbar navbar-dark bg-dark navbar-expand-lg py-0">
+
+	<ul class="navbar-nav">
+		<li class="nav-item dropdown">
+			<a class="nav-link" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+				<img src="image/menupng.png" alt="menu" width="40px" height="40px">
+			</a>
+
+			<div class="dropdown-menu bg-dark" aria-labelledby="navbarDropdownMenuLink">
+				<a class="dropdown-item text-light" href="#">List</a>
+				<a class="dropdown-item text-light" href="#">Contact</a>
+				<a class="dropdown-item text-light" href="#">About</a>
+			</div>
+	</ul>
+
+	<a class="navbar-brand" href="index.php"> <img src="image/logopng.png" alt="logo" width="auto" height="60"> </a>
+
 </nav>
