096cb3e4fb245bccabe4427b266874478bc2f242
Rouel <50535192+astrolabio1417@users.noreply.github.com>
2020-02-26 10:44:16 +0800

    new dropdown

--- navbar.php
+++ navbar.php
@@ -1,17 +1,15 @@
 <nav class="navbar navbar-dark bg-dark navbar-expand-lg py-0 mb-4">
-	<ul class="navbar-nav">
-		<li class="nav-item dropdown">
-			<div class="dropdown-menu bg-dark" aria-labelledby="navbarDropdownMenuLink">
-				<a class="dropdown-item text-light" href="#">List</a>
-				<a class="dropdown-item text-light" href="#">Contact</a>
-				<a class="dropdown-item text-light" href="#">About</a>
-			</div>
-	</ul>
 	<a class="nav-link" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 		<img src="image/menupng.png" alt="menu" width="40px" height="40px">
 	</a>
 
 	<a class="navbar-brand p-0" href="index.php"><img src="image/logopng.png" alt="logo" width="auto" height="60"></a>
 
+	<div class="dropdown-menu bg-dark" aria-labelledby="navbarDropdownMenuLink">
+		<a class="dropdown-item text-light" href="worldchat.php">World Chat</a>
+		<a class="dropdown-item text-light" href="#">List</a>
+		<a class="dropdown-item text-light" href="#">Contact</a>
+		<a class="dropdown-item text-light" href="#">About</a>
+	</div>
 
 </nav>
